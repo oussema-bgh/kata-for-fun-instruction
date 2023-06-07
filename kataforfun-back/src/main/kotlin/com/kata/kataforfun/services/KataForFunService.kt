fd849020e5de6e2cab683a6e391dbfd8e96d12ad
Mohamed Oussema BEN GHORBEL <ben.ghorbel.med.oussema@gmail.com>
2023-06-07 13:37:42 +0200

    Adding the implementation of convertNumber method

--- kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
+++ kataforfun-back/src/main/kotlin/com/kata/kataforfun/services/KataForFunService.kt
@@ -6,7 +6,62 @@ import org.springframework.stereotype.Component
 class KataForFunService {
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
+        println(inputNumber);
+
+        val digitsList = inputNumber.toString().map { it.toString().toInt() }.toList()
+
+
+        val resultDivisors = StringBuilder()
+        val resultElment = StringBuilder()
+
+
+        if(0 == inputNumber % 3){
+            resultDivisors.append("Kata")
+        }
+        if(0 == inputNumber % 5){
+            resultDivisors.append("For")
+        }
+        if(3 == inputNumber ){
+            resultElment.append("Kata")
+        }
+        if(5 == inputNumber ){
+            resultElment.append("For")
+        }
+        if (7 == inputNumber) {
+            resultElment.append("Fun")
+        }
+
+        if(digitsList.size>1) {
+            for (element in digitsList) {
+
+                println("element $element");
+                println("result_Divisors $resultDivisors");
+                println("result_Elment $resultElment");
+
+
+                if (0 == element % 3) {
+                    resultDivisors.append("Kata")
+                }
+                if (0 == element % 5) {
+                    resultDivisors.append("For")
+                }
+                if (7 == element) {
+                    resultElment.append("Fun")
+                }
+            }
+        }
+        println("result_Divisors version prefinal $resultDivisors");
+        println("result_Elment version final $resultElment");
+
+
+        resultDivisors.append(resultElment.toString());
+        println("result_Divisors final $resultDivisors");
+
+        return if (resultDivisors.isEmpty()) {
+            inputNumber.toString()
+        } else {
+            return resultDivisors.toString()
+        }
     }
 
 }
