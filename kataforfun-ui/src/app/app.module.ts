26b844f0bafffe035db62b5824c5363779230b93
Mohamed Oussema BEN GHORBEL <ben.ghorbel.med.oussema@gmail.com>
2023-06-07 18:37:31 +0200

    Add a new root in the navbar

--- kataforfun-ui/src/app/app.module.ts
+++ kataforfun-ui/src/app/app.module.ts
@@ -25,7 +25,8 @@ import { BackEndInstructionComponent } from './back-end-instruction/back-end-ins
     BrowserModule, ReactiveFormsModule, HttpClientModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'kata-for-fun', component:KataForFunComponent}
     ])
   ],
   providers: [
--- kataforfun-ui/src/app/nav-bar/nav-bar.component.ts
+++ kataforfun-ui/src/app/nav-bar/nav-bar.component.ts
@@ -7,7 +7,7 @@ import { ItemWithPath } from '../model/nav-item';
 })
 export class NavBarComponent implements OnInit {
 
-  navItems: ItemWithPath[] = [{text: 'Accueil', path: 'home'}];
+  navItems: ItemWithPath[] = [{text: 'Accueil', path: 'home'},{text: 'Kata For Fun', path: 'kata-for-fun'}];
 
   constructor() { }
 
